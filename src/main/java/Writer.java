efbb166f7024ed7918b22e0ff85ee5238d397311
Jad Baz <jbaz@activeidentity.com>
2017-04-28 01:43:46 +0300

    Randomized message generation

--- src/main/java/Writer.java
+++ src/main/java/Writer.java
@@ -1,10 +1,24 @@
+import java.util.Random;
+
 public class Writer implements Runnable{
-    private static final String MSG = new String(new char[Config.MESSAGE_CHARACTER_LENGTH]).replace("\0", "A")+Config.MESSAGE_DELIMITER;
+    static final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    static final Random rnd = new Random();
+
+    static String randomString(int len){
+        StringBuilder sb = new StringBuilder( len );
+        for( int i = 0; i < len; i++ )
+            sb.append( AB.charAt( rnd.nextInt(AB.length()) ) );
+        return sb.toString();
+    }
+
+//    private static final String MSG = new String(new char[Config.MESSAGE_CHARACTER_LENGTH]).replace("\0", "A")+Config.MESSAGE_DELIMITER;
+    private String MSG;
     @Override
     public void run() {
         try {
             long x = 0;
             while (true) {
+                MSG = randomString(Config.MESSAGE_CHARACTER_LENGTH)+Config.MESSAGE_DELIMITER;
                 Main.write(MSG);
                 if (x % Config.WRITE_BATCH_SIZE == 0) {
                     Thread.sleep(Config.SLEEP_BETWEEN_BATCHES_MILLIS);
