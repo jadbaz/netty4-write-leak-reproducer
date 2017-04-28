2fcc5f8ef25cbd3ee2ca2d07548485071309211a
Jad Baz <jbaz@activeidentity.com>
2017-04-28 14:46:55 +0300

    Tidying getting config from system properties

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,9 +1,9 @@
 public class Config {
-    public static final int PORT = System.getProperty("test.port") != null ? Integer.getInteger("test.port") : 45310;
-    public static final int MESSAGE_CHARACTER_LENGTH = System.getProperty("test.length") != null ? Integer.getInteger("test.length") : 500;
-    public static final int WRITE_BATCH_SIZE = System.getProperty("test.batch") != null ? Integer.getInteger("test.batch") : 10;
-    public static final int SLEEP_BETWEEN_BATCHES_MILLIS = System.getProperty("test.sleep") != null ? Integer.getInteger("test.sleep") : 1;
-    public static final int MEMORY_LOG_INTERVAL_MILLIS = System.getProperty("test.log_time") != null ? Integer.getInteger("test.log_time") : 10*1000;
+    public static final int PORT = Integer.getInteger("test.port",45310);
+    public static final int MESSAGE_CHARACTER_LENGTH = Integer.getInteger("test.length",500);
+    public static final int WRITE_BATCH_SIZE = Integer.getInteger("test.batch",10);
+    public static final int SLEEP_BETWEEN_BATCHES_MILLIS = Integer.getInteger("test.sleep",1);
+    public static final int MEMORY_LOG_INTERVAL_MILLIS = Integer.getInteger("test.log_time",10*1000);
     public static final String MESSAGE_DELIMITER = "\n";
     public static final boolean GENERATE_RANDOM_STRING = Boolean.getBoolean("test.random");
 }
